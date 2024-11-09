cce14669ece007f42c372f08b53488321e378fab
Jonas S <jonas1111@gmail.com>
2024-11-09 16:36:10 +0100

    Update usb capture decoder

--- usb_capture_decode/usb_hid_decoder.py
+++ usb_capture_decode/usb_hid_decoder.py
@@ -1,65 +1,84 @@
 #!/usr/bin/env python3
 import argparse
-import os.path
 from scapy.all import *
 
 usb_hid_keymap = {
-    4: 'a',
-    5: 'b',
-    6: 'c',
-    7: 'd',
-    8: 'e',
-    9: 'f',
-    10: 'g',
-    11: 'h',
-    12: 'i',
-    13: 'j',
-    14: 'k',
-    15: 'l',
-    16: 'm',
-    17: 'n',
-    18: 'o',
-    19: 'p',
-    20: 'q',
-    21: 'r',
-    22: 's',
-    23: 't',
-    24: 'u',
-    25: 'v',
-    26: 'w',
-    27: 'x',
-    28: 'y',
-    29: 'z',
-    30: '1',
-    31: '2',
-    32: '3',
-    33: '4',
-    34: '5',
-    35: '6',
-    36: '7',
-    37: '8',
-    38: '9',
-    39: '0',
-    40: 'Enter',
-    41: 'esc',
-    42: 'del',
-    43: 'tab',
+    4: ['a', 'A'],
+    5: ['b', 'B'],
+    6: ['c', 'C'],
+    7: ['d', 'D'],
+    8: ['e', 'E'],
+    9: ['f', 'F'],
+    10: ['g', 'G'],
+    11: ['h', 'H'],
+    12: ['i', 'I'],
+    13: ['j', 'J'],
+    14: ['k', 'K'],
+    15: ['l', 'L'],
+    16: ['m', 'M'],
+    17: ['n', 'N'],
+    18: ['o', 'O'],
+    19: ['p', 'P'],
+    20: ['q', 'Q'],
+    21: ['r', 'R'],
+    22: ['s', 'S'],
+    23: ['t', 'T'],
+    24: ['u', 'U'],
+    25: ['v', 'V'],
+    26: ['w', 'W'],
+    27: ['x', 'X'],
+    28: ['y', 'Y'],
+    29: ['z', 'Z'],
+    30: ['1', '!'],
+    31: ['2', '@'],
+    32: ['3', '#'],
+    33: ['4', '$'],
+    34: ['5', '%'],
+    35: ['6', '^'],
+    36: ['7', '&'],
+    37: ['8', '*'],
+    38: ['9', '('],
+    39: ['0', ')'],
+    40: '<Enter>',
+    41: '<esc>',
+    42: '<del>',
+    43: '<tab>',
     44: ' ',
-    45: '-',
-    47: '{',
-    48: '}',
-    56: '/',
+    45: ['-', '_'],
+    46: ['=', '+'],
+    47: ['[', '{'],
+    48: [']', '}'],
+    49: ['\\', '|'],
+    50: ['#', '~'],
+    51: [';', ':'],
+    52: ['\'', '"'],
+    53: ['`', '~'],
+    54: [',', '<'],
+    55: ['.', '>'],
+    56: ['/', '?'],
     57: 'CapsLock',
     79: 'RightArrow',
-    80: 'LetfArrow'
+    80: 'LetfArrow',
+    88: '<enter>',
+    89: '1',
+    90: '2',
+    91: '3',
+    92: '4',
+    93: '5',
+    94: '6',
+    95: '7',
+    96: '8',
+    97: '9',
+    98: '0',
+    99: '.',
 }
 
 
 def read_packets(pcap):
     try:
         return rdpcap(pcap)
-    except:
-        print('[-] Couldn\'t read capture file')
+    except Exception as e:
+        print(f'[-] Couldn\'t read capture file, {e}')
         exit(-1)
 
 
@@ -68,7 +87,18 @@ def read_values(packets):
     for packet in packets:
         value = packet.load[-6]
         if value in usb_hid_keymap:
-            text += usb_hid_keymap[value]
+            modifier = packet.load[-8]
+            val = usb_hid_keymap[value]
+            if modifier == 32:
+                if len(val) == 2:
+                    text += val[1]
+                else:
+                    text += val
+            else:
+                if len(val) == 2:
+                    text += val[0]
+                else:
+                    text += val
     return text
 
 
@@ -77,6 +107,7 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--pcap', help='Packet capture file',
                         type=ascii, required=True)
     args = parser.parse_args()
+    pcap = args.pcap.replace("'", "")
 
     packets = read_packets(args.pcap.replace("'", ''))
     text = read_values(packets)
